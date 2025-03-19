cdf24b5bef2a8541cd6ab961ec1e7004a4da4568
magicsong <songxuetao@bytedance.com>
2025-03-19 17:48:54 +0800

    init project

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  title: 'app for me',
   description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
+    'This is my personal application for managing tasks and projects.'
 };
 
 export default function RootLayout({
--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import 'server-only';
 
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import {
   pgTable,
   text,
@@ -14,7 +14,11 @@ import {
 import { count, eq, ilike } from 'drizzle-orm';
 import { createInsertSchema } from 'drizzle-zod';
 
-export const db = drizzle(neon(process.env.POSTGRES_URL!));
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL
+});
+
+export const db = drizzle(pool);
 
 export const statusEnum = pgEnum('status', ['active', 'inactive', 'archived']);
 
