738e75581624a1d81b95bed2a2658f7a92109275
magicsong <songxuetao@bytedance.com>
2025-04-01 18:51:20 +0800

    Revert "增加聊天机器人"
    This reverts commit f42caad39615ea73c2ad6646cb985858ebf91c71.

--- app/(dashboard)/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Chat } from "@/components/ai/chat";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col size-full items-center">
-      <Chat />
-    </div>
-  );
-}
